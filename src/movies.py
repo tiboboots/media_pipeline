5481a97d48fe240a312873a0968e22a4e58ad98d
Tibo <tiboboots@outlook.com>
2025-05-08 20:27:38 +0200

    Updated watched_movies list to be dictionary containing list of movie dictionaries as value for Movies key, this is the proper valid format used with POST http requests

--- src/movies.py
+++ src/movies.py
@@ -1,13 +1,12 @@
 import csv
+from classes import APICall
 
 letterboxd_watched= "letterboxd_data/watched.csv"
 
 with open(letterboxd_watched, "r", encoding = 'utf-8') as movies_csv:
     # Add all movies from csv to watched_movies list as a dictionary
-    watched_movies = []
+    watched_movies = {"Movies": []}
     movies = csv.DictReader(movies_csv, delimiter = ";")
     for movie in movies:
         del movie['Letterboxd URI'] # Get rid of URI key, as it is not relevant
-        watched_movies.append(movie)
-
-print(watched_movies)
+        watched_movies["Movies"].append(movie) # append each movie dictionary to list in watched_movies
