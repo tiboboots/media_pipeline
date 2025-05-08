5bd4fb738d48baab9e2bb8c2b531c774b5a96530
Tibo <tiboboots@outlook.com>
2025-05-08 12:38:39 +0200

    Added save_response method to apicall class to save api response to json file

--- src/classes.py
+++ src/classes.py
@@ -1,12 +1,14 @@
 import requests
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
 class APICall:
     def __init__(self, endpoint, parameters):
         self.api_key = os.getenv("tmdb_api_key")
+        self.file_path = "response.json"
         self.endpoint = endpoint
         self.parameters = parameters.copy()
         self.parameters['api_key'] = self.api_key
@@ -22,3 +24,7 @@ class APICall:
             print(f"Connection error: {e}")
         except requests.exceptions.Timeout as t:
             print(f"Request timeout: {t}")
+    def save_response(self, json_response):
+        with open(self.file_path, "w") as json_file:
+            json.dump(json_response, json_file, indent = 4)
+            print("Successfully saved response to json file.")
